cf1286a4328ddd419d5253514d73fddb2127504d
Shawn McHenry <drraccoony@gmail.com>
2025-03-06 16:34:41 -0600

    Rename dark-mode.js to darkmode.js in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/dark-mode.js',
+                'resources/js/darkmode.js',
             ],
             refresh: true,
         }),
